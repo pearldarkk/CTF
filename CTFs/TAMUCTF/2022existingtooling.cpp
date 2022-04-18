7a52abd34ae935fccecf10f1cb6731146b6efcd0
pearldarkk <ng.ahn.pd@gmail.com>
2022-04-19 03:45:27 +0700

    update new files

---
+++ CTFs/TAMUCTF/2022existingtooling.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+#define EL printf("\n")
+#define pY printf("YES\n")
+#define pN printf("NO\n")
+#define fi first
+#define se second
+#define MOD 1000000007LL
+#define INF 1e9
+#define LINF 1e18
+#define ld long double
+#define ll long long
+#define ull unsigned long long
+typedef pair<int, int> ii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ii> vii;
+typedef vector<string> vs;
+
+int main() {
+    freopen("text.inp", "r", stdin);
+    // freopen("text.out", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    char bytes[73] = {186, 184, 14, 128, 166, 82, 110, 24, 130, 178, 28, 151, 162, 70, 114, 6, 130, 181, 0, 129, 148, 80, 104, 0, 130, 162, 29, 132, 191, 64, 100, 20, 177, 189, 16, 186, 164, 91, 88, 17, 164, 191, 8, 136, 162, 74, 102, 25, 177, 168, 54, 151, 174, 95, 98, 7, 174, 180, 54, 149, 162, 71, 96, 42};
+    for (int i = 0; i < 73; ++i)
+
+        return 0;
+}
